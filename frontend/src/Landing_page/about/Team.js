847c04f3f7b9febeea1a224ab7c910beab109de9
Om Ahire <okahire2006@gmail.com>
2026-09-17 04:36:08 +0530

    Make Team section responsive

--- frontend/src/Landing_page/about/Team.js
+++ frontend/src/Landing_page/about/Team.js
@@ -3,27 +3,47 @@ import React from 'react';
 function Team() {
     return ( 
         <div className='container'>
-            <h4  className='text-muted' style={{marginLeft:"15cm"}}>People</h4>
-            <div className='row' style={{marginLeft:"3cm"}}>
-                <div className='col-4 p-5'>
-                    <img src='media/images/nithinKamath.jpg' style={{borderRadius:"100%" , width:"8cm"}}/>
+            <h4 className='text-muted text-center mt-5'>People</h4>
+
+            <div className='row justify-content-center'>
+                <div className='col-12 col-md-4 p-3 p-md-5 text-center'>
+                    <img
+                        src='media/images/nithinKamath.jpg'
+                        style={{
+                            borderRadius: "100%",
+                            width: "8cm",
+                            maxWidth: "100%",
+                            height: "auto"
+                        }}
+                        alt='Nithin Kamath'
+                    />
                     <br/>
                     <br/>
                     <h6 className='text-center'>Nithin Kamath</h6>
                     <p className='text-center'>Founder, CEO</p>
                 </div>
-                <div className='col-7 p-5'>
+
+                <div className='col-12 col-md-7 p-3 p-md-5'>
                     <br/>
-                    <p  className='text-muted'>Nithin bootstrapped and founded Zerodha in 2010 to overcome the hurdles he faced during his decade long stint as a trader. Today, Zerodha has changed the landscape of the Indian broking industry</p>
-                    <p  className='text-muted'>He is a member of the SEBI Secondary Market Advisory Committee (SMAC) and the Market Data Advisory Committee (MDAC).</p>
-                    <p  className='text-muted'>Playing basketball is his zen.</p>
-                    <p  className='text-muted'>Connect on <a style={{textDecoration:"none",color:"#387ED1"}} href=''>HomePage</a>  /  <a style={{textDecoration:"none",color:"#387ED1",}} href=''>TradingQnA</a> /  <a style={{textDecoration:"none"}} href=''>Twitter</a></p>
+                    <p className='text-muted'>
+                        Nithin bootstrapped and founded Zerodha in 2010 to overcome the hurdles he faced during his decade long stint as a trader. Today, Zerodha has changed the landscape of the Indian broking industry
+                    </p>
+
+                    <p className='text-muted'>
+                        He is a member of the SEBI Secondary Market Advisory Committee (SMAC) and the Market Data Advisory Committee (MDAC).
+                    </p>
+
+                    <p className='text-muted'>
+                        Playing basketball is his zen.
+                    </p>
+
+                    <p className='text-muted'>
+                        Connect on <a style={{textDecoration:"none",color:"#387ED1"}} href=''>HomePage</a> / <a style={{textDecoration:"none",color:"#387ED1"}} href=''>TradingQnA</a> / <a style={{textDecoration:"none"}} href=''>Twitter</a>
+                    </p>
                 </div>
-               
-                
             </div>
         </div>
-     );
+    );
 }
 
 export default Team;
